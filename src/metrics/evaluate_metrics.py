f61e247a9296eb5811fd6620eaecbad970b5c61c
Sokolov Fedor <sokol_f4@mail.ru>
2023-10-20 16:56:57 +0300

    edited metrics evaluation

--- src/metrics/evaluate_metrics.py
+++ src/metrics/evaluate_metrics.py
@@ -2,14 +2,15 @@ import pickle
 import yaml
 import re
 import pandas as pd
-from src.models.catboost_recommender_v1 import Recommender_v1
+import numpy as np
+from src.models.catboost_recommender_v1 import Recommender_v1_validation
 from src.metrics import hitrate_at_k
 
 
 def evaluate_metrics(model, model_name: str = 'Recommender_v1'):
     with open('config.yaml', 'r') as file:
-            config = yaml.safe_load(file)
-            validation_path = config['validation_data_path']
+        config = yaml.safe_load(file)
+        validation_path = config['validation_data_path']
 
     validation = pd.read_csv(validation_path, sep=';', index_col=None)
 
@@ -17,12 +18,19 @@ def evaluate_metrics(model, model_name: str = 'Recommender_v1'):
     for i in range(len(validation)):
         user_id = validation.user_id.iloc[i]
         liked_posts = []
+        viewed_posts = []
 
-        for post_str in re.sub(pattern=r'(\[|\])', repl='', string=validation.liked_posts.iloc[0]).split(','):
+        for post_str in re.sub(pattern=r'(\[|\])', repl='', string=validation.liked_posts.iloc[i]).split(','):
             post_id = int(post_str.strip())
             liked_posts.append(post_id)
 
-        recs = [item['id'] for item in model.predict(user_id=user_id)]
+        for post_str in re.sub(pattern=r'(\[|\])', repl='', string=validation.viewed_posts.iloc[i]).split(','):
+            post_id = int(post_str.strip())
+            viewed_posts.append(post_id)
+
+        viewed_posts = np.array(viewed_posts)    
+
+        recs = [item['id'] for item in model.predict(user_id=user_id, viewed_posts=viewed_posts)]
         hitrate += hitrate_at_k(liked_posts=liked_posts, recommended_posts=recs)
 
     hitrate_at_5 = round(hitrate / len(validation), 3)
@@ -34,6 +42,5 @@ def evaluate_metrics(model, model_name: str = 'Recommender_v1'):
 
 
 if __name__ == '__main__':
-     model = pickle.load(open('src/models/catboost_recommender_v1/catboost_rec_v1.pkl', 'rb'))
-
-     evaluate_metrics(model=model, model_name='Recommender_v1') #hitrate@5 = 0.64
+    model_v1 = pickle.load(open('src/models/catboost_recommender_v1/validation_model_v1.pkl', 'rb'))
+    evaluate_metrics(model=model_v1, model_name='Recommender_v1') #hitrate@5 = 0.572
