00b968071fe37f6d7d36d2fa0df91150a8745112
Sokolov Fedor <sokol_f4@mail.ru>
2023-10-21 17:02:23 +0300

    edited service launching

--- run_service.sh
+++ run_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH="${PYTHONPATH}:/path/to/your/project/"
 source "$DIR/venv/bin/activate"
-python run_service.py
+python ${DIR}/static/run_service.py
